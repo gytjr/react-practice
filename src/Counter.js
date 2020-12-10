d272d368a37a4b383a4e09275630a5ee1a16f844
gytjr <gytjr321@naver.com>
2020-12-10 19:45:18 +0900

    counter.js(set state)

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state; // state를 조회할 떄는 this.state로 조회한다.
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
