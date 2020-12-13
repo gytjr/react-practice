0b6ab8f24f7cf3d0e58cbd2ec35c66047a21d614
gytjr <gytjr321@naver.com>
2020-12-13 17:32:38 +0900

    useEffect

---
+++ src/Info.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
+
+  const onchangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onchangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onchangeName} />
+        <input value={nickname} onChange={onchangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
