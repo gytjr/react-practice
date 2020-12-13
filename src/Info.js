53bfee223d205eb3a7017d959765eaa7b38f78f3
gytjr <gytjr321@naver.com>
2020-12-13 22:53:21 +0900

    useReducer 인풋상태 관리하기

--- src/Info.js
+++ src/Info.js
@@ -1,29 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
-
-  const onchangeName = (e) => {
-    setName(e.target.value);
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
-  const onchangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+
   return (
     <div>
       <div>
-        <input value={name} onChange={onchangeName} />
-        <input value={nickname} onChange={onchangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
