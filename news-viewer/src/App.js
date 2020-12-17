66631f83b0b90873266f0f890f0e0a284ed62e8b
gytjr <gytjr321@naver.com>
2020-12-17 22:23:31 +0900

    뉴스api 데이터 연동하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,28 +1,9 @@
 import './App.css';
 import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
