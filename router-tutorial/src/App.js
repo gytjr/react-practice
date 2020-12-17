67499813a1e75460f9bc958bc3d2e476eee18e26
gytjr <gytjr321@naver.com>
2020-12-17 19:51:57 +0900

    리액트 라우터 부가기능 history

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -18,10 +19,14 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
---
+++ router-tutorial/src/HistorySample.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class HistorySample extends Component {
+  handleGoBack = () => {
+    this.props.history.goBack();
+  };
+
+  handleGoHome = () => {
+    this.props.history.push("/");
+  };
+
+  componentDidMount() {
+    //이것을 설정하고나면 페이지에 변화가 생기려고 할때마다 정말 나갈것인지를 질문함
+    this.unblock = this.props.history.block("정말 떠나실 건가요?");
+  }
+
+  componentWillUnmount() {
+    if (this.unblock) {
+      this.unblock();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleGoBack}>뒤로</button>
+        <button onClick={this.handleGoHome}>홈으로</button>
+      </div>
+    );
+  }
+}
+
+export default HistorySample;
