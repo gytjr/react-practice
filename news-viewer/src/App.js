a550510e35776af33924d28d6f904069b2fb7b4a
gytjr <gytjr321@naver.com>
2020-12-17 23:30:39 +0900

    라우터사용, NavLink사용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import { useCallback, useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 function App() {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />;
-    </>
-  );
+  //?가 들어가면 값이 선택적
+  return <Route path="/:category?" component={NewsPage} />;
 }
 
 export default App;
